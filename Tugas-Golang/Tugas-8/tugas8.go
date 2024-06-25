9b7b5d3b96af1947672e22210ff9612b66c4d996
adamrzky <adamrizky340@gmail.com>
2024-06-26 01:53:34 +0700

    ( fix race condition no 3 dan 4)

--- Tugas-Golang/Tugas-8/tugas8.go
+++ Tugas-Golang/Tugas-8/tugas8.go
@@ -27,7 +27,6 @@ func displayPhones(phones []string) {
 }
 
 // Soal 2
-
 func getMovies(moviesChannel chan string, movies ...string) {
 	for _, movie := range movies {
 		time.Sleep(1 * time.Second)
@@ -38,67 +37,73 @@ func getMovies(moviesChannel chan string, movies ...string) {
 
 // Soal 3
 func calculateProperties(r float64, height float64, results chan string) {
-	// Menghitung luas lingkaran
 	area := math.Pi * r * r
-	results <- fmt.Sprintf("Luas lingkaran dengan jari-jari %.2f adalah %.2f", r, area)
-
-	// Menghitung keliling lingkaran
 	circumference := 2 * math.Pi * r
-	results <- fmt.Sprintf("Keliling lingkaran dengan jari-jari %.2f adalah %.2f", r, circumference)
-
-	// Menghitung volume tabung
 	volume := area * height
+
+	results <- fmt.Sprintf("Luas lingkaran dengan jari-jari %.2f adalah %.2f", r, area)
+	results <- fmt.Sprintf("Keliling lingkaran dengan jari-jari %.2f adalah %.2f", r, circumference)
 	results <- fmt.Sprintf("Volume tabung dengan jari-jari %.2f dan tinggi %.2f adalah %.2f", r, height, volume)
 }
 
-func main() {
+// Soal 4
+func calculateArea(length, width float64, areaChan chan<- float64) {
+	areaChan <- length * width
+}
 
-	// Test Soal 1
+func calculatePerimeter(length, width float64, perimeterChan chan<- float64) {
+	perimeterChan <- 2 * (length + width)
+}
+
+func calculateVolume(length, width, height float64, volumeChan chan<- float64) {
+	volumeChan <- length * width * height
+}
+
+func main() {
 	fmt.Println("\nJawaban Soal 1:")
 	phones := []string{"Xiaomi", "Asus", "Iphone", "Samsung", "Oppo", "Realme", "Vivo"}
 	displayPhones(phones)
 
-	// Test SOal 2
-
-	var movies = []string{"Harry Potter", "LOTR", "SpiderMan", "Logan", "Avengers", "Insidious", "Toy Story"}
-
+	fmt.Println("\nJawaban Soal 2:")
+	movies := []string{"Harry Potter", "LOTR", "SpiderMan", "Logan", "Avengers", "Insidious", "Toy Story"}
 	moviesChannel := make(chan string)
-
 	go getMovies(moviesChannel, movies...)
 
-	fmt.Println("\nJawaban Soal 2:")
-	fmt.Println("List Movies:")
-	i := 1
-	for value := range moviesChannel {
-		fmt.Printf("%d. %s\n", i, value)
-		i++
+	for movie := range moviesChannel {
+		fmt.Println(movie)
 	}
 
-	// Test SOal 3
-
-	radii := []float64{8, 14, 20} // Jari-jari yang digunakan
-	height := 10.0                // Tinggi tabung
-
-	results := make(chan string, 9) // Membuat channel dengan buffer yang cukup
-	var wg sync.WaitGroup
-
-	for _, radius := range radii {
-		wg.Add(1)
-		go func(r float64) {
-			defer wg.Done()
-			calculateProperties(r, height, results)
-		}(radius)
-	}
-
-	go func() {
-		wg.Wait()
-		close(results) // Menutup channel setelah semua goroutine selesai
-	}()
-
-	// Menerima dan mencetak hasil dari channel
 	fmt.Println("\nJawaban Soal 3:")
-	for result := range results {
-		fmt.Println(result)
+	radii := []float64{8, 14, 20}
+	height := 10.0
+	results := make(chan string, 9)
+	for _, radius := range radii {
+		go calculateProperties(radius, height, results)
+	}
+	for i := 0; i < len(radii)*3; i++ {
+		fmt.Println(<-results)
 	}
 
+	fmt.Println("\nJawaban Soal 4:")
+	length := 10.0
+	width := 5.0
+	height = 3.0
+	areaChan := make(chan float64)
+	perimeterChan := make(chan float64)
+	volumeChan := make(chan float64)
+
+	go calculateArea(length, width, areaChan)
+	go calculatePerimeter(length, width, perimeterChan)
+	go calculateVolume(length, width, height, volumeChan)
+
+	for i := 0; i < 3; i++ {
+		select {
+		case area := <-areaChan:
+			fmt.Printf("Luas Persegi Panjang: %.2f\n", area)
+		case perimeter := <-perimeterChan:
+			fmt.Printf("Keliling Persegi Panjang: %.2f\n", perimeter)
+		case volume := <-volumeChan:
+			fmt.Printf("Volume Balok: %.2f\n", volume)
+		}
+	}
 }
