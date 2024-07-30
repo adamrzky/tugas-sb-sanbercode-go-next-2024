cd7aa2e946c16c24dd62172ee007eb4847967da1
adamrzky <adamrizky340@gmail.com>
2024-07-30 20:44:37 +0700

    fix vercel.go

--- Tugas-React/backend-tugas-reactjs/api/vercel.go
+++ Tugas-React/backend-tugas-reactjs/api/vercel.go
@@ -4,9 +4,9 @@ import (
 	"backend-tugas-reactjs/config"
 	"backend-tugas-reactjs/docs"
 	"backend-tugas-reactjs/routes"
-	"backend-tugas-reactjs/utils"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -19,7 +19,10 @@ var (
 func init() {
 	App = gin.New()
 
-	environment := utils.Getenv("ENVIRONMENT", "development")
+	environment := os.Getenv("ENVIRONMENT")
+	if environment == "" {
+		environment = "development"
+	}
 
 	if environment == "development" {
 		err := godotenv.Load()
@@ -31,14 +34,17 @@ func init() {
 	docs.SwaggerInfo.Title = "Movie REST API"
 	docs.SwaggerInfo.Description = "This is REST API Movie."
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = utils.Getenv("HOST", "localhost:8080")
+	docs.SwaggerInfo.Host = os.Getenv("HOST")
+	if docs.SwaggerInfo.Host == "" {
+		docs.SwaggerInfo.Host = "localhost:8080"
+	}
 	if environment == "development" {
 		docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	} else {
 		docs.SwaggerInfo.Schemes = []string{"https"}
 	}
-	db := config.ConnectDataBase()
 
+	db := config.ConnectDataBase()
 	routes.SetupRouter(db, App)
 }
 
@@ -46,34 +52,3 @@ func init() {
 func Handler(w http.ResponseWriter, r *http.Request) {
 	App.ServeHTTP(w, r)
 }
-
-// func Handler(w http.ResponseWriter, rq *http.Request) {
-// 	// for load godotenv
-// 	// for env
-// 	environment := utils.Getenv("ENVIRONMENT", "development")
-
-// 	if environment == "development" {
-// 		err := godotenv.Load()
-// 		if err != nil {
-// 			log.Fatal("Error loading .env file")
-// 		}
-// 	}
-
-// 	//programmatically set swagger info
-// 	docs.SwaggerInfo.Title = "Movie REST API"
-// 	docs.SwaggerInfo.Description = "This is REST API Movie."
-// 	docs.SwaggerInfo.Version = "1.0"
-// 	docs.SwaggerInfo.Host = utils.Getenv("HOST", "localhost:8080")
-// 	if environment == "development" {
-// 		docs.SwaggerInfo.Schemes = []string{"http", "https"}
-// 	} else {
-// 		docs.SwaggerInfo.Schemes = []string{"https"}
-// 	}
-
-// 	db := config.ConnectDataBase()
-// 	sqlDB, _ := db.DB()
-// 	defer sqlDB.Close()
-
-// 	r := routes.SetupRouter(db)
-// 	r.Run()
-// }
